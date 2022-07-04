9f88e056f53a67b06c4d8b547c9fbc938ab06eb9
spencerchubb <spencerchubb@gmail.com>
2022-07-04 01:56:52 -0400

    Improve replay

--- scripts/replay.ts
+++ scripts/replay.ts
@@ -14,36 +14,42 @@ function main() {
     const searchParams = new URLSearchParams(window.location.search);
     const solveID = parseInt(searchParams.get("solveID"));
     db.getSolve(solveID, solve => {
-        console.log(solve);
         scene.cube.setCubeState(solve.initialCubeState);
         scene.render();
 
         let moveIndex = 0;
+        let moveElements: HTMLElement[];
         const moveCounter = document.querySelector("#moveCounter");
-        function updateMoveCounter() {
+        function updateMoveCounter(newMoveIndex: number) {
+            // Revert old one to default color
+            moveElements[moveIndex].style.background = '';
+
+            moveIndex = newMoveIndex;
             moveCounter.textContent = `${moveIndex} / ${solve.moves.length}`;
+
+            // Highlight current one
+            moveElements[moveIndex].style.background = 'green';
         }
-        updateMoveCounter();
 
         document.querySelector("#leftButton").addEventListener("click", (event) => {
             if (moveIndex > 0) {
-                moveIndex--;
-                scene.cube.stepAlgorithm(solve.moves[moveIndex].move, false);
+                scene.cube.stepAlgorithm(solve.moves[moveIndex - 1].move, false);
                 scene.animateTurn();
     
-                updateMoveCounter();
+                updateMoveCounter(moveIndex - 1);
             }
         });
         document.querySelector("#rightButton").addEventListener("click", (event) => {
             if (moveIndex < solve.moves.length) {
                 scene.cube.stepAlgorithm(solve.moves[moveIndex].move, true);
                 scene.animateTurn();
-                moveIndex++;
     
-                updateMoveCounter();
+                updateMoveCounter(moveIndex + 1);
             }
         });
 
+        let absoluteStartTime;
+        let relativeStartTime;
         let paused = true;
         const playPauseButton = document.querySelector("#playPause")
         playPauseButton.addEventListener("click", () => {
@@ -51,35 +57,44 @@ function main() {
                 pause();
                 return;
             }
+            // timeOnStart = solve.moves[moveIndex].time;
+            absoluteStartTime = Date.now();
+            relativeStartTime = solve.moves[moveIndex].time;
             paused = false;
             playPauseButton.innerHTML = `
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" stroke="white" fill="white"
-                viewBox="0 0 54 54" style="enable-background:new 0 0 54 54;" xml:space="preserve">
+                viewBox="0 0 54 54" xml:space="preserve"
+                style="width: 40px; height: 40px; margin: 4px 8px;">
                 <rect x="12" y="8" width="10" height="38" />
                 <rect x="32" y="8" width="10" height="38" />
             </svg>
             `;
-            const step = () => {
+            function _setTimeout() {
+                const ms = (solve.moves[moveIndex].time - relativeStartTime) * 1000 - (Date.now() - absoluteStartTime);
+                setTimeout(step, ms);
+            }
+            function step() {
                 if (paused) return;
                 scene.cube.stepAlgorithm(solve.moves[moveIndex].move, true);
                 scene.animateTurn();
-                moveIndex++;
-                updateMoveCounter();
+                updateMoveCounter(moveIndex + 1);
                 if (moveIndex === solve.moves.length) {
                     pause();
+                    return;
                 }
 
-                setTimeout(step, (solve.moves[moveIndex].time - solve.moves[moveIndex - 1].time) * 1000);
+                _setTimeout();
             }
-            setTimeout(step, (solve.moves[moveIndex + 1].time - solve.moves[moveIndex].time) * 1000);
+            _setTimeout();
         });
 
         function pause() {
             paused = true;
             playPauseButton.innerHTML = `
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" stroke="white" fill="white"
-                viewBox="0 0 54 54" style="enable-background:new 0 0 54 54;" xml:space="preserve">
-                <polygon points="12,8 42,27 12,46" />
+                viewBox="0 0 54 54" xml:space="preserve"
+                style="width: 40px; height: 40px; margin: 4px 8px;">
+                <polygon points="15,8 45,27 15,46" />
             </svg>
             `;
         }
@@ -98,30 +113,40 @@ function main() {
 
             tr.addEventListener("click", () => {
                 if (rowIndex === moveIndex) return;
-                while (moveIndex < rowIndex) {
-                    scene.cube.stepAlgorithm(solve.moves[moveIndex].move, true);
-                    moveIndex++;
+                let newMoveIndex = moveIndex;
+                while (newMoveIndex < rowIndex) {
+                    scene.cube.stepAlgorithm(solve.moves[newMoveIndex].move, true);
+                    newMoveIndex++;
                 }
-                while (moveIndex > rowIndex) {
-                    moveIndex--;
-                    scene.cube.stepAlgorithm(solve.moves[moveIndex].move, false);
+                while (newMoveIndex > rowIndex) {
+                    newMoveIndex--;
+                    scene.cube.stepAlgorithm(solve.moves[newMoveIndex].move, false);
                 }
                 scene.cube.animationQueue = [];
                 scene.cube.commitStickers();
                 scene.render();
-                updateMoveCounter();
+                updateMoveCounter(newMoveIndex);
             });
 
             return tr;
         }
 
+        moveElements = Array(solve.moves.length);
+
         const tr = buildRow(0, "0.00", "Start");
-        solveData.appendChild(tr);
+        moveElements[0] = tr;
         
         solve.moves.forEach((move, i) => {
             const tr = buildRow(i + 1, move.time.toFixed(2), move.move);
-            solveData.appendChild(tr);
+            moveElements[i + 1] = tr;
         });
+
+        const fragment = document.createDocumentFragment();
+        moveElements.forEach(moveEle => fragment.appendChild(moveEle));
+        solveData.appendChild(fragment);
+
+        // Initial call to updateMoveCounter
+        updateMoveCounter(0);
     });
 }
 
