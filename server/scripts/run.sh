131e2ac284f11781fe947e62053d93d9dff1975d
spencerchubb <spencerchubb@gmail.com>
2022-12-05 15:20:27 +0000

    update scripts

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/go/bin
+errMsg="Please specify prod or dev for the build type. For example: sudo ./run.sh prod"
+logFile=""
 
-source ../.env
+if [ -z "$1" ]; then
+    echo $errMsg
+    exit 1
+elif [ $1 = "prod" ]; then
+    source ../.env
+    logFile=./server-prod.log
+elif [ $1 = "dev" ]; then
+    source ../.env-dev
+    logFile=./server-dev.log
+else
+    echo $errMsg
+    exit 1
+fi
 
 ./kill_port.sh $SERVER_PORT
 
-# Start the server
-go run ../
+export PATH=$PATH:/usr/local/go/bin
+
+echo $logFile
+
+# Start the server, and keep it running even if the terminal is closed
+nohup go run ../ > $logFile &
