88655fd43619ef3ff6e80cf13f5a35d0ff36a998
Spencer Chubb <spencerchubb@gmail.com>
2023-01-30 15:24:59 -0500

    generate oll and pll

--- server/algs/gen.go
+++ server/algs/gen.go
@@ -10,23 +10,66 @@ import (
 
 func main() {
 	algSets := readAlgsFromJson("./algs.json")
-	algSet := findAlgSet(algSets, "PLL")
+	for _, algSet := range algSets {
+		if algSet.Name != "OLL" {
+			continue
+		}
+		generateScramblesForAlgSet(algSet)
+	}
+	// algSet := findAlgSet(algSets, "PLL")
+	// allScrambles := SetScrambles{SetName: algSet.Name}
+	// for i, alg := range algSet.Algs {
+	// 	fmt.Printf("%d/%d: %s\n", i+1, len(algSet.Algs), alg)
+	// 	start := solver.NewCube()
+	// 	end := solver.NewCube()
+
+	// 	if algSet.OnlyOrientation != nil {
+	// 		start.SetOnlyOrientation(algSet.OnlyOrientation)
+	// 		end.SetOnlyOrientation(algSet.OnlyOrientation)
+	// 	}
+	// 	if algSet.Disregard != nil {
+	// 		start.SetDisregard(algSet.Disregard)
+	// 		end.SetDisregard(algSet.Disregard)
+	// 	}
+
+	// 	solver.PerformAlgorithm(&start, alg)
+	// 	scramblesForAlg := solver.Solve(start, end, []byte{0, 1, 2, 3, 4, 5, 15, 16, 17}, 10, 10_000, false)
+	// 	allScrambles.Scrambles = append(allScrambles.Scrambles, Scrambles{alg, scramblesForAlg})
+	// }
+	// writeScramblesToJson("./scrambles/PLL.json", allScrambles)
+}
+
+func generateScramblesForAlgSet(algSet AlgSet) {
 	allScrambles := SetScrambles{SetName: algSet.Name}
 	for i, alg := range algSet.Algs {
 		fmt.Printf("%d/%d: %s\n", i+1, len(algSet.Algs), alg)
-		cube := solver.NewCube()
-		solver.PerformAlgorithm(&cube, alg)
-		scramblesForAlg := solver.Solve(cube, []int{0, 1, 2, 3, 4, 5, 15, 16, 17}, 10, 10_000, false)
+		start := solver.NewCube()
+		end := solver.NewCube()
+
+		if algSet.OnlyOrientation != nil {
+			start.SetOnlyOrientation(algSet.OnlyOrientation)
+			end.SetOnlyOrientation(algSet.OnlyOrientation)
+		}
+		if algSet.Disregard != nil {
+			start.SetDisregard(algSet.Disregard)
+			end.SetDisregard(algSet.Disregard)
+		}
+
+		solver.PerformAlgorithm(&start, alg)
+		scramblesForAlg := solver.Solve(start, end, []byte{0, 1, 2, 3, 4, 5, 15, 16, 17}, 10, 10_000, false)
 		allScrambles.Scrambles = append(allScrambles.Scrambles, Scrambles{alg, scramblesForAlg})
 	}
-	writeScramblesToJson("./scrambles/PLL.json", allScrambles)
+	fileName := fmt.Sprintf("./scrambles/%s.json", algSet.Name)
+	writeScramblesToJson(fileName, allScrambles)
 }
 
 type AlgSet struct {
-	Cube     string   `json:"cube"`
-	Name     string   `json:"name"`
-	Inactive []int    `json:"inactive"`
-	Algs     []string `json:"algs"`
+	Cube            string   `json:"cube"`
+	Name            string   `json:"name"`
+	Inactive        []int    `json:"inactive"`
+	OnlyOrientation []int    `json:"onlyOrientation"`
+	Disregard       []int    `json:"disregard"`
+	Algs            []string `json:"algs"`
 }
 
 type Scrambles struct {
--- server/algs/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.19
-
-require github.com/spencerchubb/solver v0.0.5 // indirect
