e3c7997edbf9126fd29c33a35b9fd3bd3bba02f9
spencerchubb <spencerchubb@gmail.com>
2023-04-11 18:01:38 -0400

    remove old config and tests

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./ui/public/*.html", "./ui/src/**/*.{js,ts}"],
-  corePlugins: { preflight: false },
-  plugins: [],
-  theme: {
-    extend: {},
-  },
-}
