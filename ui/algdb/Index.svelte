cb5fe80d5c3bf057d5f16a326abc1ae818d35e14
spencerchubb <spencerchubb@gmail.com>
2023-05-06 17:58:21 -0400

    restructure algdb landing page

--- ui/algdb/Index.svelte
+++ ui/algdb/Index.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
     import SideNav from "../src/lib/components/SideNav.svelte";
     import NavBarIcon from "../src/lib/components/NavBarIcon.svelte";
-    import { algSets } from "./algSets";
     import MenuIcon from "../src/lib/components/icons/MenuIcon.svelte";
     import GithubIcon from "../src/lib/components/icons/GithubIcon.svelte";
-
-    function getAlgSetUrl(algSet: string): string {
-        return `algdb/${algSet.replaceAll(" ", "-")}.html`;
-    }
 
     let sideNavOpen = false;
 </script>
@@ -27,37 +22,87 @@
         class="col"
         style="width: 100%; gap: 16px; padding: 16px; overflow-y: auto;"
     >
-        <h1>Alg Database</h1>
+        <h1>Rubik's Cube Algorithms</h1>
         <p style="font-style: italic;">
             Open source database with 2700+ vetted algorithms
         </p>
-        {#each algSets as algSet}
-            <a
-                style="width: 100%; max-width: 400px; padding: 16px; gap: 16px;"
-                class="card row"
-                href={getAlgSetUrl(algSet.name)}
-            >
-                <p style="font-weight: bold; font-size: 1.5rem; line-height: 1.5rem;">
-                    {algSet.name}
-                </p>
-                <p style="font-size: 1.3rem;">
-                    {algSet.cases} cases
-                </p>
-            </a>
-        {/each}
+        <div class="col" style="gap: 16px;">
+            <div class="card">
+                <h2>2x2</h2>
+                <a class="alg-set" href="/algdb/2x2-PBL.html">
+                    PBL
+                </a>
+                <a class="alg-set" href="/algdb/2x2-CLL.html">
+                    CLL
+                </a>
+                <a class="alg-set" href="/algdb/2x2-EG1.html">
+                    EG1
+                </a>
+                <a class="alg-set" href="/algdb/2x2-EG2.html">
+                    EG2
+                </a>
+            </div>
+            <div class="card">
+                <h2>3x3</h2>
+                <a class="alg-set" href="/algdb/F2L.html">
+                    F2L
+                </a>
+                <a class="alg-set" href="/algdb/OLL.html">
+                    OLL
+                </a>
+                <a class="alg-set" href="/algdb/PLL.html">
+                    PLL
+                </a>
+                <a class="alg-set" href="/algdb/Winter-Variation.html">
+                    Winter Variation
+                </a>
+                <a class="alg-set" href="/algdb/COLL.html">
+                    COLL
+                </a>
+                <a class="alg-set" href="/algdb/OLLCP.html">
+                    OLLCP
+                </a>
+                <a class="alg-set" href="/algdb/ZBLL.html">
+                    ZBLL
+                </a>
+            </div>
+            <div class="card">
+                <h2>Other</h2>
+                <a class="alg-set" href="/algdb/4x4-PLL-Parity.html">
+                    4x4 PLL Parity
+                </a>
+            </div>
+        </div>
     </div>
     <SideNav bind:open={sideNavOpen} />
 </main>
 
 <style>
     .card {
-        background: transparent;
+        display: flex;
+        flex-wrap: wrap;
+        width: 300px;
         border: solid 1px var(--gray-500);
         box-shadow: 0 0 4px 2px var(--gray-600);
         border-radius: 16px;
+        padding: 16px;
+        gap: 8px 16px;
     }
 
-    .card:hover {
+    h2 {
+        width: 100%;
+        text-align: center;
+    }
+
+    .alg-set {
+        font-weight: bold;
+        font-size: 1.3rem;
+        padding: 4px;
+        border-radius: 4px;
+        color: var(--blue-300);
+    }
+
+    .alg-set:hover {
         background: var(--gray-600);
     }
 </style>
