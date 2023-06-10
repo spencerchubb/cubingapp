c3791e1c86c14c22c76bb127ba9936e1f5f8f34e
spencerchubb <spencerchubb@gmail.com>
2023-06-09 23:28:48 -0400

    fix layout

--- ui/src/index/App.svelte
+++ ui/src/index/App.svelte
@@ -13,57 +13,63 @@
     }
 </script>
 
-<div style="
-    width: 100%;
-    height: 100%;
-    padding: 32px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    overflow-y: auto;">
-    <div class="col" style="justify-content: center; gap: 16px;">
-        <h1 style="text-align: center;">CubingApp</h1>
-        <p style="text-align: center; font-size: 1.4rem; line-height: 1.8rem; font-weight: bold; font-style: italic; color: var(--gray-300);">Take speedcubing to the next level</p>
-    </div>
-    <div class="col" style="box-shadow: 0 0 4px 2px var(--gray-600);
-    border-radius: 16px; padding-bottom: 16px; margin: 16px 32px;">
-        <div style="width: 200px; height: 200px;">
-            <GlManager
-                onSceneInitialized={_scene => {
-                    scene = _scene;
-                    _scene.dragEnabled = false;
-                    _scene.enableKey = _ => false;
-                    _scene.puzzle.performAlg(scram);
-                }}
-            />
+<div style="width: 100%; height: 100%; padding: 32px; overflow-y: auto;">
+    <div class="col" style="justify-content: center;">
+        <div style="display: flex; flex-wrap: wrap; justify-content: center;">
+            <div class="col" style="justify-content: center; gap: 16px;">
+                <h1 style="text-align: center;">CubingApp</h1>
+                <p style="
+                    text-align: center;
+                    font-size: 1.4rem;
+                    line-height: 1.8rem;
+                    font-weight: bold;
+                    font-style: italic;
+                    color: var(--gray-300);">Take speedcubing to the next level</p>
+            </div>
+            <div class="col" style="
+                box-shadow: 0 0 4px 2px var(--gray-600);
+                border-radius: 16px;
+                padding-bottom: 16px;
+                margin: 16px 32px;">
+                <div style="width: 200px; height: 200px;">
+                    <GlManager
+                        onSceneInitialized={_scene => {
+                            scene = _scene;
+                            _scene.dragEnabled = false;
+                            _scene.enableKey = _ => false;
+                            _scene.puzzle.performAlg(scram);
+                        }}
+                    />
+                </div>
+                <button on:click={showSampleSolve}>Try me</button>
+            </div>
         </div>
-        <button on:click={showSampleSolve}>Try me</button>
-    </div>
-    <div class="col" style="margin-top: 32px; gap: 16px;">
-        <a href="./algdb.html">
-            <div class="blue-to-purple card">
-                <h2>Algorithm Database</h2>
-                <p>3300+ Rubik's Cube algorithms</p>
-            </div>
-        </a>
-        <a href="./recon.html">
-            <div class="red-to-orange card">
-                <h2>Reconstruction Tool</h2>
-                <p>Reconstruct solves and share with your friends</p>
-            </div>
-        </a>
-        <a href="./timer.html">
-            <div class="blue-to-purple card">
-                <h2>Timer</h2>
-                <p>Time yourself with various puzzles. 2x2, 3x3, 4x4, 5x5, 6x6, 7x7, Pyraminx</p>
-            </div>
-        </a>
-        <a href="./train.html">
-            <div class="red-to-orange card">
-                <h2>Train</h2>
-                <p>Memorize algorithms in half the time</p>
-            </div>
-        </a>
+        <div class="col" style="margin-top: 32px; gap: 16px;">
+            <a href="./algdb.html">
+                <div class="blue-to-purple card">
+                    <h2>Algorithm Database</h2>
+                    <p>3300+ Rubik's Cube algorithms</p>
+                </div>
+            </a>
+            <a href="./recon.html">
+                <div class="red-to-orange card">
+                    <h2>Reconstruction Tool</h2>
+                    <p>Reconstruct solves and share with your friends</p>
+                </div>
+            </a>
+            <a href="./timer.html">
+                <div class="blue-to-purple card">
+                    <h2>Timer</h2>
+                    <p>Time yourself with various puzzles. 2x2, 3x3, 4x4, 5x5, 6x6, 7x7, Pyraminx</p>
+                </div>
+            </a>
+            <a href="./train.html">
+                <div class="red-to-orange card">
+                    <h2>Train</h2>
+                    <p>Memorize algorithms in half the time</p>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
