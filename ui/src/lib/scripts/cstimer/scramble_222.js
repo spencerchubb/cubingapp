b1ca988fbb12ce80f86ae3dd5f131191295008bd
spencerchubb <spencerchubb@gmail.com>
2023-07-06 12:35:22 -0400

    fix scramble and remove unused

--- ui/src/lib/scripts/cstimer/scramble_222.js
+++ ui/src/lib/scripts/cstimer/scramble_222.js
@@ -2,7 +2,7 @@ import { mathlib } from "./mathlib";
 import { scrMgr } from "./scramble";
 
 export var scramble_222 = (function(rn) {
-	var solv = new mathlib.Solver(3, 3, [[0, [doPermMove, 'p', 7], 5040], [0, [doOriMove, 'o', 7, -3], 729]]);
+	var solver = new mathlib.Solver(3, 3, [[0, [doPermMove, 'p', 7], 5040], [0, [doOriMove, 'o', 7, -3], 729]]);
 
 	var movePieces = [
 		[0, 2, 3, 1],
@@ -16,8 +16,6 @@ export var scramble_222 = (function(rn) {
 		[1, 0, 1, 0, 3]
 	];
 
-	var oriCoord = new mathlib.coord('o', 7, -3);
-
 	function doPermMove(arr, m) {
 		mathlib.acycle(arr, movePieces[m]);
 	}
@@ -26,63 +24,18 @@ export var scramble_222 = (function(rn) {
 		mathlib.acycle(arr, movePieces[m], 1, moveOris[m]);
 	}
 
-	var cFacelet = [
-		[3, 4, 9],
-		[1, 20, 5],
-		[2, 8, 17],
-		[0, 16, 21],
-		[13, 11, 6],
-		[15, 7, 22],
-		[12, 19, 10]
-	];
-
-	function checkNoBar(pidx, oidx) {
-		var perm = mathlib.set8Perm([], pidx, 7);
-		var ori = oriCoord.set([], oidx);
-		var f = [];
-		for (var i = 0; i < 24; i++) {
-			f[i] = i >> 2;
-		}
-		mathlib.fillFacelet(cFacelet, f, perm, ori, 4);
-		for (var i = 0; i < 24; i += 4) {
-			if ((1 << f[i] | 1 << f[i + 3]) & (1 << f[i + 1] | 1 << f[i + 2])) {
-				return false;
-			}
-		}
-		return true;
+    function getScramble() {
+        var lim = 3, perm, ori;
+        do {
+            perm = rn(5040);
+            ori = rn(729);
+		} while (perm == 0 && ori == 0 || solver.search([perm, ori], 0, lim) != null);
+        return solver.toStr(solver.search([perm, ori], lim).reverse(), "URF", "'2 ");
 	}
 
-	function getScramble(type, length, state) {
-		var ori, perm, lim;
-		var maxl = type == '222o' ? 0 : 9;
-		do {
-			lim = 2;
-			if (type == '222o' || type == '222so') {
-				perm = rn(5040);
-				ori = rn(729);
-				lim = 3;
-			} else if (type == '222eg') {
-				ori = egmap[state & 0x7];
-				perm = [0, 2, 3, 4, 5, 1][state >> 3];
-				var arr = mathlib.set8Perm([0, 0, 0, 0].concat(egperms[perm]), rn(24), 4);
-				perm = mathlib.get8Perm(arr, 7);
-				var rndU = rn(4);
-				ori = oriCoord.set([], ori);
-				while (rndU-- > 0) {
-					doOriMove(ori, 0);
-				}
-				ori = oriCoord.get(ori);
-			} else if (/^222eg[012]$/.exec(type)) {
-				return getScramble('222eg', length, [0, 8, 40][~~type[5]] + state);
-			} else if (type == '222nb') {
-				do {
-					perm = rn(5040);
-					ori = rn(729);
-				} while (!checkNoBar(perm, ori));
-			}
-		} while (perm == 0 && ori == 0 || solv.search([perm, ori], 0, lim) != null);
-		return solv.toStr(solv.search([perm, ori], maxl).reverse(), "URF", "'2 ");
-	}
+	scrMgr.reg(['222o'], getScramble);
 
-	scrMgr.reg(['222o', '222so', '222nb'], getScramble);
+    return {
+        getScramble,
+    }
 })(mathlib.rn);
--- ui/src/train/scramble.ts
+++ ui/src/train/scramble.ts
@@ -1,8 +1,9 @@
 import { Puzzle } from "../lib/scripts/rubiks-viz/puzzle";
 import { scramble_333 } from "../lib/scripts/cstimer/scramble_333";
 import { Cube } from "../lib/scripts/rubiks-viz";
+import { scramble_222 } from "../lib/scripts/cstimer/scramble_222";
 
-const STICKERS = {
+const S = {
     UBL: 0,
     UL: 1,
     ULF: 2,
@@ -59,27 +60,27 @@ const STICKERS = {
     RDB: 53,
 };
 
-const UB = [STICKERS.UB, STICKERS.BU];
-const UL = [STICKERS.UL, STICKERS.LU];
-const UR = [STICKERS.UR, STICKERS.RU];
-const UF = [STICKERS.UF, STICKERS.FU];
-const DB = [STICKERS.DB, STICKERS.BD];
-const DL = [STICKERS.DL, STICKERS.LD];
-const DR = [STICKERS.DR, STICKERS.RD];
-const DF = [STICKERS.DF, STICKERS.FD];
-const FL = [STICKERS.FL, STICKERS.LF];
-const FR = [STICKERS.FR, STICKERS.RF];
-const BL = [STICKERS.BL, STICKERS.LB];
-const BR = [STICKERS.BR, STICKERS.RB];
+const UB = [S.UB, S.BU];
+const UL = [S.UL, S.LU];
+const UR = [S.UR, S.RU];
+const UF = [S.UF, S.FU];
+const DB = [S.DB, S.BD];
+const DL = [S.DL, S.LD];
+const DR = [S.DR, S.RD];
+const DF = [S.DF, S.FD];
+const FL = [S.FL, S.LF];
+const FR = [S.FR, S.RF];
+const BL = [S.BL, S.LB];
+const BR = [S.BR, S.RB];
 
-const UBL = [STICKERS.UBL, STICKERS.BLU, STICKERS.LUB];
-const ULF = [STICKERS.ULF, STICKERS.LFU, STICKERS.FUL];
-const URB = [STICKERS.URB, STICKERS.RBU, STICKERS.BUR];
-const UFR = [STICKERS.UFR, STICKERS.FRU, STICKERS.RUF];
-const DFL = [STICKERS.DFL, STICKERS.FLD, STICKERS.LDF];
-const DLB = [STICKERS.DLB, STICKERS.LBD, STICKERS.BDL];
-const DRF = [STICKERS.DRF, STICKERS.RFD, STICKERS.FDR];
-const DBR = [STICKERS.RDB, STICKERS.BRD, STICKERS.RDB];
+const UBL = [S.UBL, S.LUB, S.BLU];
+const ULF = [S.ULF, S.FUL, S.LFU];
+const URB = [S.URB, S.BUR, S.RBU];
+const UFR = [S.UFR, S.RUF, S.FRU];
+const DFL = [S.DFL, S.FLD, S.LDF];
+const DLB = [S.DLB, S.LBD, S.BDL];
+const DRF = [S.DRF, S.RFD, S.FDR];
+const DBR = [S.DBR, S.BRD, S.RDB];
 
 // BR, BL, FL, FR, DB, DL, DF, DR, UL, UB, UF, UR
 // Order of min2phase edges (M2PE)
@@ -115,7 +116,6 @@ const gl = (document.querySelector("canvas") as HTMLCanvasElement).getContext("w
 const threeByThree = new Cube(gl, [], 3);
 
 export function scramble(puzzle: Puzzle, alg: string): string {
-
     // If the puzzle is a 2x2, we do something special.
     // Perform the alg on a 3x3, then disregard the edges.
     if ((puzzle as Cube).layers === 2) {
@@ -123,42 +123,44 @@ export function scramble(puzzle: Puzzle, alg: string): string {
         threeByThree.solve();
         threeByThree.performAlg(alg);
 
-        // With short 2x2 algorithms, the scramble can be too simple sometimes.
-        // So we perform a few more moves to make it more interesting.
-        threeByThree.performAlg("M E S M2 E");
+        const anyEdges = 0xffffffffffff;
+        let [cp, co] = getPermAndOri(puzzle, corners, 8);
+        return scramble_333.getAnyScramble(anyEdges, anyEdges, cp, co, null, null, null, null);
     }
 
-    let [ep, eo, cp, co] = get_EP_EO_CP_CO(puzzle);
-
-    console.log({
-        ep, eo, cp, co
-    })
+    let [ep, eo] = getPermAndOri(puzzle, edges, 12);
+    let [cp, co] = getPermAndOri(puzzle, corners, 8);
 
     return scramble_333.getAnyScramble(ep, eo, cp, co, null, null, null, null);
 }
 
-function get_EP_EO_CP_CO(puzzle: Puzzle): [number[], number[], number[], number[]] {
-    let ep: number[] = [];
-    let eo: number[] = [];
-    let cp: number[] = [];
-    let co: number[] = [];
+function getPermAndOri(puzzle: Puzzle, pieces: number[][], numPieces: number): [number[], number[]] {
+    let perm = new Array(numPieces);
+    let ori = new Array(numPieces);
 
     const stickers = puzzle.stickers;
-    edges.forEach(edge => {
-        const i = findPiece(puzzle, edge, edges);
-        ep.push(i);
-        const sticker = stickers[edge[0]];
-        eo.push(edges[i].findIndex(e => e === sticker));
-    });
+    for (let i = 0; i < numPieces; i++) {
+        const piece = findPiece(puzzle, pieces[i], pieces);
+        perm[i] = piece;
+        ori[i] = pieces[i].findIndex(e => {
+            const sticker = stickers[e];
+            const isWhite = 0 <= sticker && sticker < 9;
+            const isYellow = 18 <= sticker && sticker < 27;
+            return isWhite || isYellow;
+        });
+        if (ori[i] === -1 && pieces[i].length == 2) {
+            // No white or yellow sticker was found, and the length is 2.
+            // That means this is an edge in the E slice.
+            const edgeSticker = pieces[i][0];
+            ori[i] = 
+                (edgeSticker === S.FL
+                || edgeSticker === S.FR
+                || edgeSticker === S.BL
+                || edgeSticker === S.BR) ? 0 : 1;
+        }
+    }
 
-    corners.forEach(corner => {
-        const i = findPiece(puzzle, corner, corners);
-        cp.push(i);
-        const sticker = stickers[corner[0]];
-        co.push(corners[i].findIndex(e => e === sticker));
-    });
-
-    return [ep, eo, cp, co];
+    return [perm, ori];
 }
 
 function findPiece(puzzle: Puzzle, piece: number[], pieces: number[][]): number {
@@ -179,20 +181,26 @@ function pieceIsThere(puzzle: Puzzle, piece1: number[], piece2: number[]): boole
 
 /** Two arrays are pseudo equal if they contain the same elements in any order. */
 function pseudoEqual(arr1: number[], arr2: number[]): boolean {
-    const freq1 = computeFreq(arr1);
-    const freq2 = computeFreq(arr2);
-    for (const key in freq1) {
-        if (freq1[key] !== freq2[key]) {
-            return false;
-        }
-    }
-    return true;
+    if (arr1.length !== arr2.length) return false;
+    return arr1.every(element => arr2.includes(element));
 }
 
-function computeFreq(arr: number[]): Object {
-    const freq = {};
-    arr.forEach(e => {
-        freq[e] = (freq[e] || 0) + 1;
-    });
-    return freq;
-}
+// TODO remove if not needed
+// function pseudoEqual(arr1: number[], arr2: number[]): boolean {
+//     const freq1 = computeFreq(arr1);
+//     const freq2 = computeFreq(arr2);
+//     for (const key in freq1) {
+//         if (freq1[key] !== freq2[key]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function computeFreq(arr: number[]): Object {
+//     const freq = {};
+//     arr.forEach(e => {
+//         freq[e] = (freq[e] || 0) + 1;
+//     });
+//     return freq;
+// }
