41bbb4439baf47a3659b1568902846182a43a7d4
spencerchubb <spencerchubb@gmail.com>
2023-08-24 12:05:46 -0400

    remove unused shuffle function

--- ui/src/lib/scripts/util.ts
+++ ui/src/lib/scripts/util.ts
@@ -2,33 +2,6 @@ import type { TrainingAlg } from "./api/algSet";
 import { randInt } from "./common/rand";
 
 /**
- * Fisher-Yates shuffle: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
- * 
- * Mutates original array and returns the shuffled array
- * 
- * @param array 
- * @returns shuffled array
- */
-export function shuffle(array: any[]) {
-    let i = array.length;
-
-    // While there remain elements to shuffle...
-    while (i != 0) {
-
-        // Pick a remaining element...
-        let r = randInt(i);
-        i--;
-
-        // And swap it with the current element.
-        let temp = array[r];
-        array[r] = array[i];
-        array[i] = temp;
-    }
-    
-    return array;
-}
-
-/**
  * Series produces 2, 5, 9, 14, 20, 27, 35, 44, 54, 65, 77, 80...
  */
 export function series(n: number) {
