0151032216b156b51b105529c5b007df14ee4170
spencerchubb <spencerchubb@gmail.com>
2023-09-01 15:25:05 -0400

    fix once bug to improve perf

--- ui/src/lib/scripts/rubiks-viz/once.ts
+++ ui/src/lib/scripts/rubiks-viz/once.ts
@@ -3,7 +3,8 @@ type OnceFunction<Arg, Result> = (arg: Arg) => Result;
 export function once<Arg, Result>(fn: OnceFunction<Arg, Result>): OnceFunction<Arg, Result> {
     let result: Result;
     return function () {
-        result = result || fn.apply(this, arguments);
+        // or true in case the function returns void
+        result = result || fn.apply(this, arguments) || true;
         return result;
     };
 }
