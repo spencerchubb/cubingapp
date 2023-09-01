d7650e44034b3ced35d0aba974e7ade2f9a31c6c
spencerchubb <spencerchubb@gmail.com>
2023-09-01 14:48:33 -0400

    update pbl description

--- ui/algdb/sveltePages/2x2-PBL.svelte
+++ ui/algdb/sveltePages/2x2-PBL.svelte
@@ -7,10 +7,20 @@
     <div>
         <h1>2x2 PBL Algorithms</h1>
         <p>
-            PBL stands for Permutation of Both Layers. It is a good intermediat emethod for 2x2 solvers after they have learned a beginner method.
+            PBL stands for Permutation of Both Layers, and it is the third step of the Ortega method.
+            Ortega is a good intermediate method for 2x2 solvers who have already learned the beginner method.
         </p>
         <p>
-            Instead of solving a layer, you can begin by solving a face. That means the first layer doesn't have to be permuted, which leads to more efficient solutions for the first step. Then you orient the last layer, and finally permuate both layers at the same time.    
+            Here is how the Ortega method works:
+        </p>
+        <p>
+            Typically in the beginner method, you start with the first layer.
+            In the Ortega method, you just start with a face, or in other words, the first layer doesn't have to be permuted correctly.
+            The second step of Ortega is to orient the last layer, and lastly, you do PBL to finish the 2x2.
+        </p>
+        <p>
+            PBL only has 6 cases, and you may already know 2 of the cases from other methods: J Perm and Y Perm.
+            For this reason, PBL is a very easy algorithm set to learn.
         </p>
         <h2>Cubers also use</h2>
         <a href="/algdb/2x2-CLL.html" class="link">
