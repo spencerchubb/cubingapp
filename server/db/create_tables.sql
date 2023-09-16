b346d24ddd03580c90c640efa05fc1524303bdb7
spencerchubb <spencerchubb@gmail.com>
2023-09-16 13:39:27 -0400

    remove timer from schema

--- server/db/create_tables.sql
+++ server/db/create_tables.sql
@@ -8,26 +8,6 @@ CREATE TABLE "alg_sets" (
     "name" varchar NOT NULL DEFAULT "set",
 );
 
-CREATE TABLE "sessions" (
-    "id" serial PRIMARY KEY NOT NULL,
-    "uid" integer NOT NULL,
-    "name" varchar NOT NULL,
-    "updated" timestamp NOT NULL,
-    "deleted" timestamp
-);
-
-CREATE TABLE "solves" (
-    "id" serial PRIMARY KEY NOT NULL,
-    "uid" integer NOT NULL,
-    "timestamp" timestamp NOT NULL DEFAULT NOW(),
-    "time" numeric NOT NULL,
-    "scramble" varchar NOT NULL,
-    "moves" varchar NOT NULL,
-    "puzzle" varchar NOT NULL,
-    "session_id" integer NOT NULL,
-    "penalty" varchar NOT NULL DEFAULT ''
-);
-
 CREATE TABLE "users" (
     "uid" serial PRIMARY KEY NOT NULL,
     "email" varchar NOT NULL,
