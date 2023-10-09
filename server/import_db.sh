628fa634bcd43b4b7a607f0a4df5bbe9d4123bce
spencerchubb <spencerchubb@gmail.com>
2023-10-08 23:41:05 -0400

    update db

---
+++ server/import_db.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Get the link for the tsv from this website:
+# https://www.worldcubeassociation.org/export/results
+
+# Get url from first param
+url=$1
+
+# Get folder name from url
+src_dir=${url##*/}
+
+# If not downloaded already, download the zip file
+if [ ! -f "$src_dir" ]; then
+    wget $url
+fi
+
+unzip "$src_dir" -d WCA_export
+rm "$src_dir"
+
+# Loop through the .tsv files in the source directory
+cd WCA_export
+for file in *.tsv; do
+    # Remove WCA_export_ from the filename
+    new_filename=${file#WCA_export_}
+    echo "$file" "$new_filename"
+    mv "$file" "$new_filename"
+done
+rm championships.tsv
+rm eligible_country_iso2s_for_championship.tsv
+rm Formats.tsv
+rm Results.tsv
+rm RoundTypes.tsv
+rm Scrambles.tsv
+cd ..
+
+# Remove the old database
+rm src/wca.db
+
+# Convert the .tsv files to a sqlite database
+csvs-to-sqlite WCA_export/*.tsv "src/wca.db" -s $'\t'
+
+sqlite3 src/wca.db < ./setup.sql
--- server/src/php/wca_attribution.php
+++ server/src/php/wca_attribution.php
@@ -1,3 +1,3 @@
 <p style="font-size: 1rem; text-align: center; align-self: center; color: var(--gray-300);">
-    Based on WCA data as of Oct 1, 2023
+    Based on WCA data as of Oct 9, 2023
 </p>
