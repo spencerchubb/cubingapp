9be534096286cd9c273247b96f5ce6fa1e235a97
spencerchubb <spencerchubb@gmail.com>
2023-12-16 22:02:29 -0500

    redesign sub-x calculator

--- server/src/am-i-sub-x.php
+++ server/src/am-i-sub-x.php
@@ -21,6 +21,16 @@
         line-height: 1.5rem;
         margin-top: 16px;
     }
+
+    .output-card {
+        width: 300px;
+        font-size: 1.5rem;
+        line-height: 2rem;
+        border: solid 1px var(--gray-500);
+        border-radius: 8px;
+        padding: 16px;
+        box-shadow: 0 2px 8px -2px var(--gray-500);
+    }
 </style>
 
 <body style="width: 100%; height: 100%;">
@@ -34,33 +44,52 @@
             <h2 class="subheader">How do I know if I'm sub-X?</h2>
             <p class="bodytext">
                 Have you ever wondered if you are sub-10, sub-15, sub-20, etc?
-                Typically cubers say that an average of 100 solves is enough to know if you are sub-X.
+                Is 100 solves enough to know if you are sub-X?
             </p>
             <p class="bodytext">
-                The right number of solves is a matter of opinion.
-                Some people say that you need 500 or even 1000 solves to be sure.
-            </p>
-            <h2 class="subheader">A better way</h2>
-            <p class="bodytext">
-                This tool uses a statistical formula called 'confidence intervals'.
-            </p>
-            <p class="bodytext">
-                In any average, there is a chance that you got lucky or unlucky, so we can never know the "true" average with 100% confidence.
-                The confidence score tells us how confident we are that the "true" average is sub-X.
+                Some people say that you need 100, 500 or even 1000 solves to be sure.
+                There is a better way to know if you are sub-X on the Rubik's Cube.
+                This tool uses statistics to calculate confidence.
             </p>
             <div style="margin-top: 32px;"></div>
             <div id="upload-timer-data"></div>
-            <div class="row" style="margin-top: 16px; gap: 16px;">
-                <p style="margin: 0; font-weight: bold; align-self: center;">Solves</p>
-                <input
-                    id="num-solves-input"
-                    type="number"
-                    min="1"
-                />
-            </div>
+            <p style="margin-top: 32px; font-weight: bold; align-self: center;">Number of Solves</p>
+            <input
+                style="margin-top: 8px; width: 250px;"
+                id="num-solves-input"
+                type="number"
+                min="1"
+            />
         </div>
-        <div id="confidence-data-div"></div>
-        <div style="margin-top: 32px;">
+        <div id="outputs" style="
+            margin-top: 32px;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: 16px;"></div>
+        <div class="col" style="width: 100%; max-width: 600px;">
+            <h2 class="subheader">How does this tool work?</h2>
+            <p class="bodytext">
+                This tool uses a statistical formula called 'confidence intervals' to determine how confident we are in determing your true average.
+            <p>
+            <p class="bodytext">
+                In a Rubik's Cube solve, there is some variability.
+                Sometimes you get lucky and get a good scramble, and sometimes you get a bad PLL case.
+                This can make it difficult to know your true average.
+            </p>
+            <p class="bodytext">
+                Confidence intervals tell us how confident we are that the true average is below a certain number.
+                Due to the variability in solves, we can never be 100% confident.
+            </p>
+            <p class="bodytext">
+                Including more solves in the calculation can make us more confident.
+                However, most solvers improve over time, so we allow you to choose how many of your recent solves to include.
+            </p>
+            <p class="bodytext">
+                I hope this tool helps you, since asking "Am I sub-X?" is one of the most common questions cubers have!
+            </p>
+        </div>
+        <div style="margin-top: 64px;">
             <?php include "php/cool_calculators.php" ?>
         </div>
         <div style="margin-top: 96px;"></div>
@@ -151,46 +180,41 @@
             const avg = calcAvg(times);
             const std = calcStd(times, avg);
 
-            const upperBound = z => avg + z * std / Math.sqrt(times.length);
-
-            const eighty = upperBound(1.282);
-            const ninety = upperBound(1.645);
-            const ninetyFive = upperBound(1.96);
-            const ninetyNine = upperBound(2.576);
+            const z = 1.64; // 95% of the standard normal distribution is below 1.64
 
             return {
                 name: session.sessionName,
-                eighty: eighty.toFixed(2),
-                ninety: ninety.toFixed(2),
-                ninetyFive: ninetyFive.toFixed(2),
-                ninetyNine: ninetyNine.toFixed(2),
+                upper: avg + z * std / Math.sqrt(times.length),
             };
         });
     }
 
+    function formatTime(seconds) {
+        if (isNaN(seconds)) return "NaN";
+        if (seconds < 60) return seconds.toFixed(2);
+        else if (seconds < 3600) {
+            const minutes = Math.floor(seconds / 60);
+            const secondsLeft = seconds % 60;
+            return `${minutes}:${secondsLeft.toFixed(2).padStart(5, "0")}`;
+        }
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secondsLeft = seconds % 60;
+        return `${hours}:${minutes.toFixed(0).padStart(2, "0")}:${secondsLeft.toFixed(2).padStart(5, "0")}`;
+    }
+
     function renderConfidenceData() {
         const confidenceIntervals = computeConfidenceIntervals(state.data, state.numSolves);
-
-        const div = document.querySelector("#confidence-data-div");
+        console.log(confidenceIntervals)
+        const div = document.querySelector("#outputs");
         div.innerHTML = "";
-        div.appendChild(E("table", { style: "margin-top: 32px;" }, [
-            E("thead", {}, [
-                E("tr", {}, [
-                    E("th", { textContent: "Session" }),
-                    E("th", { textContent: "80%" }),
-                    E("th", { textContent: "90%" }),
-                    E("th", { textContent: "95%" }),
-                    E("th", { textContent: "99%" }),
-                ]),
-            ]),
-            E("tbody", {}, confidenceIntervals.map(interval => E("tr", {}, [
-                E("td", { textContent: interval.name }),
-                E("td", { textContent: interval.eighty }),
-                E("td", { textContent: interval.ninety }),
-                E("td", { textContent: interval.ninetyFive }),
-                E("td", { textContent: interval.ninetyNine }),
-            ]))),
-        ]));
+        confidenceIntervals.forEach(interval => {
+            div.appendChild(E("p", { className: "output-card" }, [
+                E("p", {
+                    textContent: `${interval.name}: 95% confident your average is below ${formatTime(interval.upper)}`,
+                })
+            ]));
+        });
     }
 
     const uploadBox = document.querySelector("#upload-timer-data");
@@ -203,7 +227,7 @@
     );
 
     const numSolvesInput = document.querySelector("#num-solves-input");
-    numSolvesInput.value = 100;
+    numSolvesInput.value = state.numSolves;
     numSolvesInput.onchange = event => {
         state.numSolves = event.target.value;
         renderConfidenceData();
