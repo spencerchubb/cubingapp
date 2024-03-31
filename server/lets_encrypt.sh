2fd59899aab4ea25e246f31757e5d6ba9d1267d1
spencerchubb <spencerchubb@gmail.com>
2024-03-31 13:23:44 -0400

    update lets ecnrypt command

--- server/lets_encrypt.sh
+++ server/lets_encrypt.sh
@@ -10,4 +10,4 @@ WILDCARD=*.$DOMAIN
 
 echo $DOMAIN && echo $WILDCARD
 
-sudo certbot -d $DOMAIN -d $WILDCARD --manual --preferred-challenges=dns certonly
+sudo certbot -d $DOMAIN -d $WILDCARD --manual --preferred-challenges=dns certonly --key-type ecdsa
