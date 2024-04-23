b11322c41e454204938d6581d9bfb9d8d2371741
spencerchubb <spencerchubb@gmail.com>
2024-04-23 15:00:00 -0400

    name ranks: first, last, or any

--- server/src/name-ranks/index.php
+++ server/src/name-ranks/index.php
@@ -33,12 +33,6 @@ function setUrlParam(key, value) {
     else urlParams.delete(key);
     window.location.search = urlParams.toString();
 }
-
-function goToPage(page, pages) {
-    page = Math.max(1, page);
-    page = Math.min(pages, page);
-    setUrlParam('page', page);
-}
 </script>
 
 <body style="display: flex; flex-direction: column; width: 100%; height: 100%;">
@@ -75,6 +69,7 @@ function goToPage(page, pages) {
             $name = $_GET["name"];
             $event = $_GET["event"] ?? "333";
             $type = $_GET["type"] ?? "Single";
+            $nameType = $_GET["nameType"] ?? "any"; // Options are first, last, any
             $page = $_GET["page"] ?? 1;
             $perPage = 20;
         ?>
@@ -94,122 +89,145 @@ function goToPage(page, pages) {
                     }
                 ?>
             </select>
+            <select id="nameType" style="width: 100%; max-width: 300px;">
+                <?php
+                    foreach([["first", "First name exact"], ["last", "Last name exact"], ["any", "Any name part"]] as $element) {
+                        echo createOption($element[0], $element[1], $nameType);
+                    }
+                ?>
+            </select>
             <script>
                 q("#name").addEventListener("change", (event) => setUrlParam("name", event.target.value));
                 q("#event").addEventListener("change", (event) => setUrlParam("event", event.target.value));
                 q("#type").addEventListener("change", (event) => setUrlParam("type", event.target.value));
+                q("#nameType").addEventListener("change", (event) => setUrlParam("nameType", event.target.value));
             </script>
         </div>
         <div style="margin-top: 24px;"></div>
         <?php
-        if (!$name) {
-            return;
-        }
+        if ($name) {
+            include_once "../php/wca_attribution.php";
 
-        include_once "../php/wca_attribution.php";
+            error_reporting(E_ALL);
+            ini_set("display_errors", 1);
+            $db = new SQLite3("/wca.db");
 
-        error_reporting(E_ALL);
-        ini_set("display_errors", 1);
-        $db = new SQLite3("/wca.db");
-
-        if (!$db) {
-            die("Error connecting to the database: " . $db->lastErrorMsg());
-        }
-
-        $query = "select personId, name, best
-            from Ranks$type
-            where name like :name and eventId = :event
-            limit 1000;";
-        $stmt = $db->prepare($query);
-        $stmt->bindValue(":name", "%$name%", SQLITE3_TEXT);
-        $stmt->bindValue(":event", $event, SQLITE3_TEXT);
-
-        $rows = $stmt->execute();
-
-        echo "<div class='table-wrapper' style='margin: 1rem auto;'><table>";
-        echo "<tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Score</th>
-        </tr>";
-
-        function timeToStr($centiseconds) {
-            $h = 360000;
-            $m = 6000;
-            $hours = floor($centiseconds / $h);
-            $minutes = floor(($centiseconds % $h) / $m);
-            $seconds = floor((($centiseconds % $h) % $m) / 100);
-            $milliseconds = $centiseconds % 100;
-        
-            // 02d means 2 digits with leading zeros if necessary.
-            if ($hours) {
-                return sprintf('%d:%02d:%02d.%02d', $hours, $minutes, $seconds, $milliseconds);
-            } else if ($minutes) {
-                return sprintf('%d:%02d.%02d', $minutes, $seconds, $milliseconds);
+            if (!$db) {
+                die("Error connecting to the database: " . $db->lastErrorMsg());
             }
-            return sprintf('%d.%02d', $seconds, $milliseconds);
-        }
 
-        $fmcScoreToStr = [
-            "Single" => function ($score) {
-                return $score;
-            },
-            "Average" => function ($score) {
-                return $score / 100;
-            },
-        ][$type];
+            $query = "select personId, name, best
+                from Ranks$type
+                where (name like :name or name like :name1) and eventId = :event
+                limit 1000;";
 
-        function mbldScoreToStr($value) {
-            $seconds = floor($value / 100) % 1e5;
-            $centiseconds = $seconds === 99999 ? null : $seconds * 100;
-            
-            $points = 99 - (floor($value / 1e7) % 100);
-            $missed = $value % 100;
-            $solved = $points + $missed;
-            $attempted = $solved + $missed;
-            return sprintf('%d/%d %s', $solved, $attempted, timeToStr($centiseconds));
-        }
+            $stmt = $db->prepare($query);
+            if ($nameType === "first") {
+                // Example: "Jon" matches "Jon Doe" but not "Jonathan Doe"
+                $stmt->bindValue(":name", "$name %", SQLITE3_TEXT);
+            } else if ($nameType === "last") {
+                $stmt->bindValue(":name", "% $name", SQLITE3_TEXT);
+            } else {
+                $stmt->bindValue(":name", "%$name%", SQLITE3_TEXT);
+            }
+            $stmt->bindValue(":name1", $name, SQLITE3_TEXT);
+            $stmt->bindValue(":event", $event, SQLITE3_TEXT);
 
-        $scoreToStrFunction = [
-            "333" => "timeToStr",
-            "222" => "timeToStr",
-            "444" => "timeToStr",
-            "555" => "timeToStr",
-            "666" => "timeToStr",
-            "777" => "timeToStr",
-            "333bf" => "timeToStr",
-            "333fm" => $fmcScoreToStr,
-            "333oh" => "timeToStr",
-            "clock" => "timeToStr",
-            "minx" => "timeToStr",
-            "pyram" => "timeToStr",
-            "skewb" => "timeToStr",
-            "sq1" => "timeToStr",
-            "444bf" => "timeToStr",
-            "555bf" => "timeToStr",
-            "333mbf" => "mbldScoreToStr",
-        ][$event];
+            $rows = $stmt->execute();
 
-        $i = 1;
-        while ($row = $rows->fetchArray(SQLITE3_ASSOC)) {
-            $personId = $row["personId"];
-            $name = $row["name"];
-            $best = $scoreToStrFunction($row["best"]);
-
+            echo "<div class='table-wrapper' style='margin: 1rem auto;'><table>";
             echo "<tr>
-                <td>$i</td>
-                <td>
-                    <a class='link' href='https://worldcubeassociation.org/persons/$personId'>$name</a>
-                </td>
-                <td>$best</td>
+                <th>#</th>
+                <th>Name</th>
+                <th>Score</th>
             </tr>";
-            $i++;
+
+            function timeToStr($centiseconds) {
+                $h = 360000;
+                $m = 6000;
+                $hours = floor($centiseconds / $h);
+                $minutes = floor(($centiseconds % $h) / $m);
+                $seconds = floor((($centiseconds % $h) % $m) / 100);
+                $milliseconds = $centiseconds % 100;
+            
+                // 02d means 2 digits with leading zeros if necessary.
+                if ($hours) {
+                    return sprintf('%d:%02d:%02d.%02d', $hours, $minutes, $seconds, $milliseconds);
+                } else if ($minutes) {
+                    return sprintf('%d:%02d.%02d', $minutes, $seconds, $milliseconds);
+                }
+                return sprintf('%d.%02d', $seconds, $milliseconds);
+            }
+
+            $fmcScoreToStr = [
+                "Single" => function ($score) {
+                    return $score;
+                },
+                "Average" => function ($score) {
+                    return $score / 100;
+                },
+            ][$type];
+
+            function mbldScoreToStr($value) {
+                $seconds = floor($value / 100) % 1e5;
+                $centiseconds = $seconds === 99999 ? null : $seconds * 100;
+                
+                $points = 99 - (floor($value / 1e7) % 100);
+                $missed = $value % 100;
+                $solved = $points + $missed;
+                $attempted = $solved + $missed;
+                return sprintf('%d/%d %s', $solved, $attempted, timeToStr($centiseconds));
+            }
+
+            $scoreToStrFunction = [
+                "333" => "timeToStr",
+                "222" => "timeToStr",
+                "444" => "timeToStr",
+                "555" => "timeToStr",
+                "666" => "timeToStr",
+                "777" => "timeToStr",
+                "333bf" => "timeToStr",
+                "333fm" => $fmcScoreToStr,
+                "333oh" => "timeToStr",
+                "clock" => "timeToStr",
+                "minx" => "timeToStr",
+                "pyram" => "timeToStr",
+                "skewb" => "timeToStr",
+                "sq1" => "timeToStr",
+                "444bf" => "timeToStr",
+                "555bf" => "timeToStr",
+                "333mbf" => "mbldScoreToStr",
+            ][$event];
+
+            $i = 1;
+            while ($row = $rows->fetchArray(SQLITE3_ASSOC)) {
+                $personId = $row["personId"];
+                $name = $row["name"];
+                $best = $scoreToStrFunction($row["best"]);
+
+                echo "<tr>
+                    <td>$i</td>
+                    <td>
+                        <a class='link' href='https://worldcubeassociation.org/persons/$personId'>$name</a>
+                    </td>
+                    <td>$best</td>
+                </tr>";
+                $i++;
+            }
+
+            $db->close();
+
+            echo "</table></div>";
         }
-
-        $db->close();
-
-        echo "</table></div>";
         ?>
+        <div class="info-div">
+            <h2>Name type options:</h2>
+            <ul>
+                <li><span class="bold">First name exact:</span> Match if the first name is exactly the same. For example, "jon" matches "Jon Doe" but not "Jonathan Doe".</li>
+                <li><span class="bold">Last name exact:</span> Match if the last name is exactly the same. For example, "smith" matches "Alice Smith" but not "Alice Smithson".</li>
+                <li><span class="bold">Any name part:</span> Match if any part of the name is the same. For example, "son" matches "Jackson Brown".</li>
+            </ul>
+        </div>
         <div style="margin-top: 96px;"></div>
     </main>
 </body>
@@ -223,6 +241,23 @@ function goToPage(page, pages) {
 
     table tr:nth-child(even) {
         background: var(--gray-900);
+    }
+
+    .info-div {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        max-width: 600px;
+        gap: 16px;
+        margin: 0 auto;
+    }
+
+    .info-div h2 {
+        margin-top: 32px;
+    }
+
+    .bold {
+        font-weight: bold;
     }
 </style>
 
