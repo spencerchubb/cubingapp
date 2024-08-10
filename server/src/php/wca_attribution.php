944772524135de2fd76fcc68e96262aaa09bf644
spencerchubb <spencerchubb@gmail.com>
2024-08-09 22:49:19 -0500

    update export date programmatically

--- server/src/php/wca_attribution.php
+++ server/src/php/wca_attribution.php
@@ -1,3 +1,9 @@
 <p style="font-size: 1rem; text-align: center; align-self: center; color: var(--gray-300);">
-    Based on WCA data as of Oct 9, 2023
+    WCA data from <?php
+        $db = new SQLite3("/wca.db");
+        $stmt = $db->prepare("SELECT * FROM Miscellaneous WHERE key = 'export_date';");
+        $rows = $stmt->execute();
+        $row = $rows->fetchArray();
+        echo $row["value"];
+    ?>
 </p>
