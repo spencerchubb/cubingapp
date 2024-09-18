b11624d7c2731a51923e9660462f41744bc90a7b
spencerchubb <spencerchubb@gmail.com>
2024-09-17 19:32:35 -0500

    blog

---
+++ server/src/blog/is-speedcubing-a-sport/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="icon" href="/assets/favicon.svg" type="image/x-icon">
+    <title>Is speedcubing a sport?</title>
+</head>
+
+<html>
+
+<style>
+body {
+    padding: 16px;
+    overflow-y: auto;
+
+    & * {
+        max-width: 600px;
+    }
+}
+
+h1 {
+    margin: auto;
+}
+
+h2 {
+    margin: 40px auto 16px auto;
+}
+
+p {
+    margin: 16px auto;
+    line-height: 1.8rem;
+}
+
+ul, ol {
+    margin: auto;
+}
+
+li {
+    margin: auto;
+    line-height: 1.8rem;
+}
+
+a {
+    color: var(--lightBlue-500);
+
+    &:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+}
+
+.tldr {
+    font-size: 18px;
+    padding: 16px;
+    margin: 16px auto;
+    color: var(--gray-100);
+    background: #181818;
+    border-radius: 16px;
+}
+</style>
+
+<body>
+    <h1>Is speedcubing a sport?</h1>
+
+    <p class="tldr">
+        TLDR:
+        You can argue that speedcubing is a sport because of mental fitness, competitions, and the ability to make friends.
+        On the other hand, you can argue it is not a sport because it is not physical.
+    </p>
+
+    <p>
+        Many people have debated whether speedcubing is a sport.
+        This debate is complicated by the fact that different organizations have different definitions for the term <i>sport</i>.
+    </p>
+    <p>
+        In this blog post, I lay out a couple definitions and analyze whether speedcubing is a sport.
+        I look at both sides of the debate to give a fair analysis.
+    </p>
+
+    <h2>What is a sport?</h2>
+    <p>
+        There is some disagreement on the exact definition of a sport.
+        For instance, here is <a href="https://web.archive.org/web/20111028112912/http://www.sportaccord.com/en/members/index.php?idIndex=32&idContent=14881">SportAccord's definition</a>. A sport should:
+    </p>
+    <ol>
+        <li>have an element of competition ✅</li>
+        <li>be in no way harmful to any living creature ✅</li>
+        <li>not rely on equipment provided by a single supplier ✅</li>
+        <li>not rely on any "luck" element specifically designed into the sport ❌</li>
+    </ol>
+    <p>
+        Speedcubing meets the first three criteria.
+        However, you could argue that it does not meet the fourth criteria.
+        There is some luck in speedcubing, because a lucky scramble or a lucky solution can give you a better result.
+        Because of this, SportAccord may not consider speedcubing to be a sport.
+    </p>
+    <p>
+        Furthermore, SportAccord says that an activity with limited physicality or athleticism should be carefully evaluated.
+        Speedcubing has a bit of a physical aspect, because you have to turn fast and use good fingertricks.
+        But compared to other sports, speedcubing is much less physical.
+    </p>
+    <p>
+        We should not only look at one definition, so let's see <a href="https://search.coe.int/cm/Pages/result_details.aspx?ObjectID=09000016804c9dbb">Council of Europe's definition</a>.
+    </p>
+    <ol>
+        <li>physical activity ❌</li>
+        <li>expresses or improves physical fitness and mental well-being ✅</li>
+        <li>forms social relationships ✅</li>
+        <li>or obtaining results in competitions ✅</li>
+    </ol>
+    <p>
+        Speedcubing does not meet the first criteria, but does meet the other three criteria.
+        Since speedcubing does not a highly physical activity, Council of Europe may not call speedcubing a sport.
+    </p>
+    <h2>Speedcubing Competitions</h2>
+    <p>
+        Most people would agree that a sport needs to have some competitions, rules, and maybe even a governing body.
+        The World Cube Assocation (WCA) is the official organization that holds competitions.
+        (By the way, if you want to go to a competition, check out the <a href="/competitions">competitions near you</a>)
+    </p>
+    <p>
+        In addition, speedcubers often want to know how they stack up against other cubers.
+        The <a href="/ranks">cubing ranks</a> show who are the fastest cubers for any country and any event.
+    </p>
+    <p>
+        Since speedcubers are ranked against each other, that is a point in favor of calling speedcubing a sport.
+    </p>
+
+    <h2>Is speedcubing an Olympic sport?</h2>
+    <p>
+        As of 2024, speedcubing has never been an Olympic sport.
+    </p>
+    <p>
+        The Olympics sometimes include temporary sports.
+        The purpose of this is to get more people in the Olympics, especially young people.
+    </p>
+    <p>
+        If speedcubing continues to grow, I hope it can become an Olympic sport, even as a temporary addition.
+    </p>
+
+    <h2>Is speedcubing a mind sport?</h2>
+    <p>
+        The International Olympic Committee recognizes chess as a sport even though players primarily compete with their mind.
+        In these games, the players sit at a board, and the only movement required is to move the pieces.
+        This category is called <i>mind sports</i>.
+        Mind sports can be controversial, because not every organization includes mind sports in their definition of a sport.
+    </p>
+    <p>
+        Speedcubing falls somewhere between a physical sport and a mind sport.
+        The physical aspect is the turning and fingertricks.
+        The mind aspect is inspection, lookahead, and learning algorithms.
+    </p>
+
+    <h2>Esports</h2>
+    <p>
+        An esport is a type of sport using video games.
+        There is a growing movement to recognize esports as sports.
+    </p>
+    <p>
+        There are many formats of esports.
+        For example, the goal of speedrunning is to complete something in the fastest time.
+        Speedcubing is most similar to the speedrunning format.
+        There are other formats such as team formats, or games where you compete to get the highest score.
+    </p>
+    <p>
+        Speedcubing is not an esport because it is not a video game, but the rising popularity of esports can shift the debate on whether cubing is a sport.
+        A video game typically has no physical component aside from a keyboard and mouse.
+        So if a video game can be a sport, then speedcubing can be a sport too.
+    </p>
+
+    <h2>Conclusion</h2>
+    <p>
+        When considering all factors, I categorize speedcubing as a sport.
+        Speedcubing is a great way to stimulate your mind, make friends, and accomplish a goal.
+        But due to its non-athletic nature, not everyone agrees.
+    </p>
+    <p>
+        At the end of the day, the question is mostly philosophical.
+        Even if you don't think it's a sport, speedcubers will continue to enjoy it for a long time.
+    </p>
+
+    <div style="margin-top: 150px;"></div>
+</body>
+
+<?php include "../../php/gtag.php" ?>
+
+</html>
