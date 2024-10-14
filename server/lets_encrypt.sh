d1d8e1cd284283c508698cb676b34aa963ff11d2
spencerchubb <spencerchubb@gmail.com>
2024-10-14 13:26:05 -0500

    restart apache

--- server/lets_encrypt.sh
+++ server/lets_encrypt.sh
@@ -8,3 +8,6 @@ WILDCARD=*.$DOMAIN
 echo $DOMAIN && echo $WILDCARD
 
 sudo certbot -d $DOMAIN -d $WILDCARD --manual --preferred-challenges=dns certonly --key-type ecdsa
+
+# Need to restart apache sinc the cert files have changed
+sudo service apache2 restart
