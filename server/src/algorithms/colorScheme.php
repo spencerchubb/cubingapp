e0345cb5a24da23082385896a0c1540ddd087952
spencerchubb <spencerchubb@gmail.com>
2025-01-18 23:03:21 -0600

    update colors

--- server/src/algorithms/colorScheme.php
+++ server/src/algorithms/colorScheme.php
@@ -1,19 +1,18 @@
 <?php
 
-// TODO: find+replace
-// --u-face: #ff0;
-// --d-face: #fff;
-// --f-face: #00f;
-// --b-face: #0f0;
-// --l-face: #f70;
-// --r-face: #f00;
+$white = "#fff";
+$yellow = "#ff0";
+$blue = "#00f";
+$green = "#0f0";
+$orange = "#f70";
+$red = "#f00";
 $colorSchemes = [
-    "white" => "* { --u-face: yellow; --d-face: white; --f-face: blue; --b-face: green; --l-face: orange; --r-face: red }",
-    "yellow" => "* { --u-face: white; --d-face: yellow; --f-face: green; --b-face: blue; --l-face: orange; --r-face: red; }",
-    "green" => "* { --u-face: blue; --d-face: green; --f-face: white; --b-face: yellow; --l-face: orange; --r-face: red; }",
-    "blue" => "* { --u-face: green; --d-face: blue; --f-face: yellow; --b-face: white; --l-face: orange; --r-face: red; }",
-    "orange" => "* { --u-face: red; --d-face: orange; --f-face: white; --b-face: yellow; --l-face: blue; --r-face: green; }",
-    "red" => "* { --u-face: orange; --d-face: red; --f-face: white; --b-face: yellow; --l-face: green; --r-face: blue; }",
+    "white" => "* { --u-face: $yellow; --d-face: $white; --f-face: $blue; --b-face: $green; --l-face: $orange; --r-face: $red; }",
+    "yellow" => "* { --u-face: $white; --d-face: $yellow; --f-face: $green; --b-face: $blue; --l-face: $orange; --r-face: $red; }",
+    "green" => "* { --u-face: $blue; --d-face: $green; --f-face: $white; --b-face: $yellow; --l-face: $orange; --r-face: $red; }",
+    "blue" => "* { --u-face: $green; --d-face: $blue; --f-face: $yellow; --b-face: $white; --l-face: $orange; --r-face: $red; }",
+    "orange" => "* { --u-face: $red; --d-face: $orange; --f-face: $white; --b-face: $yellow; --l-face: $blue; --r-face: $green; }",
+    "red" => "* { --u-face: $orange; --d-face: $red; --f-face: $white; --b-face: $yellow; --l-face: $green; --r-face: $blue; }",
 ];
 $bottomColor = $_COOKIE["bottomColor"] ?? "white";
 $colorScheme = $colorSchemes[$bottomColor];
