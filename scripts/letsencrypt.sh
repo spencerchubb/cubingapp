0e72d8eed6eefc67590641bd72988d67c7485a89
spencerchubb <spencerchubb@gmail.com>
2026-03-08 17:28:54 -0500

    add sitemap to tanstack app

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -11,9 +11,3 @@ sudo certbot -d $DOMAIN -d $WILDCARD --manual --preferred-challenges=dns certonl
 
 # Need to restart apache since the cert files have changed
 sudo service apache2 restart
-
--GNR0YqsiHJNLgJ4xiRGcdkl0vndWK0UTWmVbxLMdis
-OwLNyDq9QdlH2Ye0tmV1UjoGI0_jDm9oD8UMF53YJ6c
-
-certificate: /etc/letsencrypt/live/cubingapp.com/fullchain.pem
-key: /etc/letsencrypt/live/cubingapp.com/privkey.pem
