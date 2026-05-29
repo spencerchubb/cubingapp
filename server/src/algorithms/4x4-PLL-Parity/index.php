84ddc53efb10460017483b934c7df3339dacb318
MavenTheAI <maventheai@gmail.com>
2026-05-29 02:29:56 -0700

    Fix typo on 4x4 PLL parity page

--- server/src/algorithms/4x4-PLL-Parity/index.php
+++ server/src/algorithms/4x4-PLL-Parity/index.php
@@ -21,7 +21,7 @@ include_once "../colorScheme.php";
     <main>
         <div style="width: 100%; max-width: 700px; height: 100%;">
             <h1>4x4 PLL Parity Algorithms</h1>
-            <p class="bodytext">When you are solving a 4x4, PLL parity can significantly slow down a solve. Luckily, there are some tricks you can use to minimize the damage without learning too mamny new algorithms.</p>
+            <p class="bodytext">When you are solving a 4x4, PLL parity can significantly slow down a solve. Luckily, there are some tricks you can use to minimize the damage without learning too many new algorithms.</p>
 			<p class="bodytext">Some algorithms in this set are similar to PLL algorithms you already know, except there is a parity algorithm mixed in. By doing the parity algorithm strategically, you can turn a slow PLL into a fast PLL.</p>
             
             <?php
